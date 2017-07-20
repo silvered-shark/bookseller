f694ee242d4c57fbdfd6b6319dc2e961b6c57f37
sachinluthra36 <sachinluthra36@gmail.com>
2017-07-20 16:39:36 +0530

    Few files added

---
+++ routes/sell.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/product',function (req, res, next) {
+
+});
+
+router.get('/books',function (req, res, next) {
+
+});
