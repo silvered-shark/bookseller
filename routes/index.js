71bffbb748466c2a1b6fe8881723fd5b5bbecff5
sachinluthra36 <sachinluthra36@gmail.com>
2017-07-24 10:52:07 +0530

    Fixed fb auth

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function (router, passport) {
-router.use(passport.initialize());
+
+    router.use(passport.initialize());
+router.use(passport.session());
+
 router.get('/', function(req, res, next) {
   res.json({"Hello":"Sachin"});
 });
 
 
-router.get('/auth/facebook', passport.authenticate('facebook',{scope: 'email'}));
+router.get('/auth/facebook', passport.authenticate('facebook',{ scope: 'email' }));
 
 router.get('/auth/facebook/callback',
     passport.authenticate('facebook', { successRedirect: '/profile',
         failureRedirect: '/' }));
 
 router.get('/profile',function (req, res, next) {
-    res.json({user:res.user});
+
 });
 
 router.post('/login',function (req, res, next) {
