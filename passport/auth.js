e60e5576c9cffd4f14891aadf5f1be25ef4675e7
Sachin Luthra <sachinluthra36@gmail.com>
2017-08-23 03:55:06 +0000

    Update auth.js

--- passport/auth.js
+++ passport/auth.js
@@ -5,12 +5,12 @@ module.exports = {
     'facebookAuth' : {
         'clientID' : '448726735491201',
         'clientSecret' : '84a4e93d0e57c7857e442271bd5021b1',
-        'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL' : 'http://sellbooks.site/auth/facebook/callback'
     },
     'GmailAuth' : {
         'clientID' : '699524736885-m55q0qp03lf781r9f4kb08iqm2jogaml.apps.googleusercontent.com',
         'clientSecret' : 'uEAUSXce-YiBYZ_6yF8bX0cZ',
-        'callbackURL' : 'http://localhost:3000/auth/google/callback'
+        'callbackURL' : 'http://sellbooks.site/auth/google/callback'
 
     }
 
